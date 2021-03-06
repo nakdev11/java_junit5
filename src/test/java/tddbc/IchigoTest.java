5fbecea6ecff4739e2641bcf5c7fe3b88a92b023
kurashimatomoya <t-kurashima@holmescloud.com>
2021-03-06 12:51:43 +0900

    テストコードのリファクタリング テストコード階層化 green

--- src/test/java/tddbc/IchigoTest.java
+++ src/test/java/tddbc/IchigoTest.java
@@ -2,29 +2,35 @@ package tddbc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class IchigoTest {
 
-    @Test
-    public void 品種を文字列で与えてあまおうであればいちごクラスに保存() {
-        // Setup
-        Ichigo sut = new Ichigo();
-        // Exercise
-        sut.setHinsyu("あまおう");
-        String actual = sut.getHinsyu();
-        // Verify
-        assertEquals("あまおう", actual);
+
+    @Nested
+    class 品種とサイズを与えていちごを作成する{
+        @Test
+        public void 品種を文字列で与えてあまおうであればいちごクラスに保存() {
+            // Setup
+            Ichigo sut = new Ichigo();
+            // Exercise
+            sut.setHinsyu("あまおう");
+            String actual = sut.getHinsyu();
+            // Verify
+            assertEquals("あまおう", actual);
+        }
+
+        @Test
+        public void 品種を文字列で与えてとちおとめであればいちごクラスに保存() {
+            // Setup
+            Ichigo sut = new Ichigo();
+            // Exercise
+            sut.setHinsyu("とちおとめ");
+            String actual = sut.getHinsyu();
+            // Verify
+            assertEquals("とちおとめ", actual);
+        }
     }
 
-    @Test
-    public void 品種を文字列で与えてとちおとめであればいちごクラスに保存() {
-        // Setup
-        Ichigo sut = new Ichigo();
-        // Exercise
-        sut.setHinsyu("とちおとめ");
-        String actual = sut.getHinsyu();
-        // Verify
-        assertEquals("とちおとめ", actual);
-    }
 }
