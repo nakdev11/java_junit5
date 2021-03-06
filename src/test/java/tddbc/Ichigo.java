c4fea50b373727654fa051b60cf0bef6c5a3d6bc
nakdev11 <nak200ok@gmail.com>
2021-03-06 12:35:56 +0900

    greenにした

--- src/test/java/tddbc/Ichigo.java
+++ src/test/java/tddbc/Ichigo.java
@@ -1,7 +1,16 @@
 package tddbc;
 
 public class Ichigo {
+
     private String hinsyu;
+
+	public String getHinsyu() {
+		return hinsyu;
+	}
+
+	public void setHinsyu(String hinsyu) {
+		this.hinsyu = hinsyu;
+	}
 
 
 }
