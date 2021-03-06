3e9b95c18c2d770ec85a8cd34f556c1cbffcb90d
nakdev11 <nak200ok@gmail.com>
2021-03-06 14:19:10 +0900

    文字列表現のテストコードを追加 res

--- src/test/java/tddbc/IchigoTest.java
+++ src/test/java/tddbc/IchigoTest.java
@@ -2,6 +2,7 @@ package tddbc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -68,6 +69,24 @@ public class IchigoTest {
             Ichigo sut = new Ichigo();
             // Verify
             assertThrows(Exception.class, () -> sut.setSize("XL"));
+        }
+
+    }
+
+    @Nested
+    class いちごの文字列表現を取得する {
+
+        @Test
+        @DisplayName("品種が登録されている場合、登録された品種を {品種}: {サイズ}　の文字列で取得できる")
+        public void 品種が登録されている場合_登録された品種を品種サイズの文字列で取得できる() throws Exception {
+        	// Setup
+        	Ichigo sut = new Ichigo();
+        	// Exercise
+        	sut.setHinsyu("あまおう");
+        	sut.setSize("M");
+        	String actual = sut.getIchigoInfoToString();
+        	// Verify
+        	assertEquals("あまおう: M", actual);
         }
 
     }
